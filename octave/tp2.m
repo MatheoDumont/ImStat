b5c7f8b0496f1975a44857c4a0d379424d5fd3e4
Matheo <math.dumont.md@gmail.com>
2021-01-20 18:55:12 +0100

    tp2 + rapport

---
+++ octave/tp2.m
@@ -0,0 +1,188 @@
+# ouvrir images
+# prendre pixel
+# calculer homography
+# utiliser vgg_warp
+# decrire les artefacts
+
+
+# calculer homography
+# https://medium.com/all-things-about-robotics-and-computer-vision/homography-and-how-to-calculate-it-8abf3a13ddc5
+function [H] = homography(matched_pixel)
+  M = [];
+  for i=1:size(matched_pixel)
+    x = matched_pixel(i, 1);
+    y = matched_pixel(i, 2);
+    xp = matched_pixel(i, 3);
+    yp = matched_pixel(i, 4);
+    v = [x y 1 0 0 0 -(xp*x) -(xp*y) -xp];
+    M = [M; v];
+    v = [0 0 0 x y 1 -(yp*x) -(yp*y) -yp];
+    M = [M; v];
+  endfor
+  [U S V] = svd(M);
+  
+  H = V(:,end);
+  H = reshape(H,3,3);
+  H = H';
+endfunction
+
+# EXERCICE 1
+# ouvrir images
+A = imread("data/regression/keble_a.jpg");
+B = imread("data/regression/keble_b.jpg");
+
+# prendre pixels
+matched_pixel = [658 287 366 289; 642 360 347 361; 681 359 386 361; 342 56 50 39];
+H = homography(matched_pixel);
+  
+# utiliser vgg_warp
+im_warped = vgg_warp_H(A, H);
+#im_warped = vgg_warp_H(B, inverse(H));
+im_warped = (im_warped/2 + B) ;
+subplot(131);
+imshow(A);
+subplot(132);
+imshow(im_warped);
+subplot(133);
+imshow(B);
+
+# construire de plus grandes images
+#NI = vgg_warp_H(A,H, 'linear', 'fit');
+#res =  zeros(size(NI, 1), size(NI, 2) * 2, 3, "uint8");
+#res(:, 1:size(NI, 2), :) = NI;
+#res(:, size(NI, 2):size(B, 2), :) = B;
+#imshow(res);
+# ca marche pas bien ... il faudrait joindre les deux bords, surement un algo comme ceux de synthetisation d'image
+
+# EXERCICE 2
+
+function [distance, choosen] = RANSAC(listofpair, A, B)
+  distance = 100000000;
+  choosen = [];
+  
+  # parametre : nombre d'iteration
+  n_iter = 10;
+  # parametre : nombre de pair selectionnees par iteration
+  n = 4;
+  
+  for i=1:n_iter
+    pick = [];
+    # pick n pair of points
+    for j=1:n
+      row = (size(listofpair)-1) * rand + 1;
+      row = int32(row);
+      pick = [pick; listofpair(row, :)];
+    endfor
+    h = homography(pick);
+    warpped = vgg_warp_H(A, h);
+    
+    cur_dis = 0;
+    # compute error / distance
+    for j=1:n
+      pos_pix_B = [pick(j, 3), pick(j, 4)];
+      
+      pix_A = warpped(int32(pos_pix_B));
+      pix_B = B(int32(pos_pix_B));
+ 
+      cur_dis += dot(pix_B - pix_A, pix_B - pix_A);
+    endfor
+    
+    if (cur_dis < distance)
+      distance = cur_dis;
+      choosen = pick;
+    endif
+    
+  endfor
+  
+endfunction
+
+pairofpoints = importdata("data/regression/matchesab.txt");
+A = imread("data/regression/keble_a.jpg");
+B = imread("data/regression/keble_b.jpg");
+
+[d worthy] = RANSAC(pairofpoints, A, B);
+H = homography(worthy);
+im_warped = vgg_warp_H(A, H);
+#im_warped = vgg_warp_H(B, inverse(H));
+subplot(231);
+imshow(A);
+subplot(232);
+imshow(im_warped);
+subplot(233);
+imshow(B);
+
+
+# Resultat exo 1 pour comparer (a afficher avec)
+subplot(235);
+matched_pixel = [658 287 366 289; 642 360 347 361; 681 359 386 361; 342 56 50 39];
+h = homography(matched_pixel);
+im_warped = vgg_warp_H(A, h);
+imshow(im_warped);
+
+# on test 10 fois pour voir a quel point les resultats peuvent varier
+for i=1:10
+  [d worthy] = RANSAC(pairofpoints, A, B);
+  H = homography(worthy);
+  im_warped = vgg_warp_H(A, H);
+  im_warped = (im_warped/2 + B);
+  #im_warped = vgg_warp_H(B, inverse(H));
+  
+  subplot(2, 5, i);
+  imshow(im_warped);
+  
+endfor
+
+
+
+
+# Exercice 2 avec seuil
+
+function [distance, choosen] = RANSAC_threshold(listofpair, A, B)
+  distance = 100000000;
+  choosen = [];
+  threshold = 200.0;
+  
+  # parametre : nombre de pair selectionnees par iteration
+  n = 4;
+  
+  while distance > threshold
+    pick = [];
+    # pick n pair of points
+    for j=1:n
+      row = (size(listofpair)-1) * rand + 1;
+      row = int32(row);
+      pick = [pick; listofpair(row, :)];
+    endfor
+    h = homography(pick);
+    warpped = vgg_warp_H(A, h);
+    
+    cur_dis = 0;
+    # compute error / distance
+    for j=1:n
+      pos_pix_B = [pick(j, 3), pick(j, 4)];
+      
+      pix_A = warpped(int32(pos_pix_B));
+      pix_B = B(int32(pos_pix_B));
+ 
+      cur_dis += dot(pix_B - pix_A, pix_B - pix_A);
+    endfor
+    
+    if (cur_dis < distance)
+      distance = cur_dis;
+      choosen = pick;
+    endif
+  endwhile
+  
+endfunction
+
+for i=1:10
+  [d worthy] = RANSAC_threshold(pairofpoints, A, B);
+  H = homography(worthy);
+  im_warped = vgg_warp_H(A, H);
+  im_warped = (im_warped/2 + B);
+  #im_warped = vgg_warp_H(B, inverse(H));
+  
+  subplot(2, 5, i);
+  imshow(im_warped);
+  
+endfor
---
+++ rapports/rapport_tp2.md
@@ -0,0 +1,60 @@
+# TP 2 : Regression
+
+Matheo Dumont p1602557
+
+You can find my source code [here on my repository](https://github.com/MatheoDumont/ImStat).
+The code for this projet is located at : 
+```
+octave/tp2.m
+```
+If you want to run it, make sure to run `data/regression/vgg_warp_h/m`, in octave, i haven't found the right command, so i used some tricky things (in octave : right-click and run).
+
+## Exercice 1 : Image registration with hand-picked pairs of points
+After building the homography between `A` and `B`, and computing the transformation
+we obtain a resulting Panorama by mixing the image A warpped that gives us `A'`, such as :
+```
+A' = vgg_warp_H(A, H)
+```
+and `B` like :
+```
+I = (A'/2 + B);
+```
+The resulting image is, with `A` on the left, in the middle the Panorama and `B` on the right :  
+![hand-picked](../images/tp2ex1.png)
+Now for the Panorama, the strong luminosity on the left is due to the accumulation of both image `A'` and `B`, although `A'` is divided.
+Also, we can see, by adding those two images, that the homography does not transform well `A`, the left part is *blurry*, because `A'` and `B` does not overlap well.
+
+We could also build a bigger image that contains A transformed relatively to B, i've made an attempt, but to rebuild the entire Panorama, i should probably use an algorithm like one used for Texture Synthesis.
+
+## Exercice 2 : Image registration with detected pairs of feature points
+I implemented the RANCAS algorithm, and used arbitrarily paremeters :
+* `n` is the number of pair we pick each iteration.
+* `n_iter` describe how many times we make to converge.
+
+For `n_iter`, i've set it to `10`. For `n`, i would think that `1` is not enough, the homography could be rotated around, with `2` it could probably be translated and still rotated but on less axis, `3` is better but still has some fuzzy effect, so i choose `4` just to be sure.  
+  
+The model used to describe the error is the squared difference of the pixel in `B` and the pixel in `A'`. 
+So for each iteration, we compute the homography `H` and transform `A` to `A'`, then using the pixel coordinates in B :
+
+```cpp
+h = homography(pick);
+warpped = vgg_warp_H(A, h);
+
+...
+
+pix_A = warpped(pos_pix_B);
+pix_B = B(pos_pix_B);
+ 
+distance(A->B) = dot(pix_B - pix_A, pix_B - pix_A);
+```
+
+For 10 call to the implemented *RANSAC* algorithm, each image is the result using the pair of points returns by *RANSAC* with the parameters introduce earlier :  
+![tp2exo2](../images/tp2exo2.png)
+
+As we can see, there's globally no aberrant result, except with the fourth image were outliers creates uncoherent images.
+
+Instead of using `n_iter`, we can use a threshold that the best distance/error mustn't exceed.
+Using an arbitrarily defined threshold set at `200.0`, for 10 call again :   
+![tp2exo22](../images/tp2exo22.png)
+This time, there is not a single outliers, all the result are seamingly good.
+But on some image, we can see some lines that doesn't align well, the stairs on the fourth and ninth images, for example.
